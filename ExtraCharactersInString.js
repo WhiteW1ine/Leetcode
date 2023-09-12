dbcd247329603650432768a0e669ef72500f709f
WhiteW1ine <foxratmir2000@gmail.com>
2023-09-12 19:08:33 +0200

    Templates

---
+++ ExtraCharactersInString.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string[]} dictionary
+ * @return {number}
+ */
+var minExtraChar = function(s, dictionary) {
+    
+};
+
+
+let s = "leetscode"
+let dictionary = ["leet","code","leetcode"]
+console.log(minExtraChar(s, dictionary))
---
+++ SplitLinkedListinParts.js
@@ -0,0 +1,15 @@
+
+
+/**
+ * @param {int} head
+ * @param {number} k
+ * @return {ListNode[]}
+ */
+var splitListToParts = function(head, k) {
+    
+};
+
+
+let arr = [1,2,3,4,5]
+let parts = 7
+
